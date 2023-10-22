0bf72f71a7fe4f57cc63d3c7f2363d498886b904
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2023-10-21 21:48:52 -0400

    Welcome Page view

---
+++ front/src/component/StatusBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const StatusBar: React.FC = () => {
+  return (
+    <img
+      src="./svg/status-bar-black.svg"
+      alt="Status bare"
+      className="status-bar"
+    />
+  );
+};
+
+export default StatusBar;
