8e35d2d94348ecd7bb2497d146d4e3aca637150d
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2023-11-02 12:20:26 -0400

    Balance page started

--- front/src/page/WelcomePage.tsx
+++ front/src/page/WelcomePage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import StatusBar from "../component/status-bar";
 
 //На цій сторінці ми створюємо верстку та розміщуємо дві
 //кнопки-посилання на сторінку /signup та сторінку /signin
@@ -15,6 +16,7 @@ const WelcomePage: React.FC = () => {
       */}
 
       <div className="welcome">
+        <StatusBar color="white" />
         <h1 className="welcome-title">Hello!</h1>
         <p className="welcome-text">Welcome to bank app</p>
       </div>
